126837edb2f48e7bbea7ebd50ceabcc70ec94ff4
Jyoti Ranjan <107979908+JyotiranjanGhibila@users.noreply.github.com>
2025-07-18 08:50:36 +0530

    login

--- src/module/users/user.route.js
+++ src/module/users/user.route.js
@@ -1,13 +1,10 @@
-
-const express = require('express');
-const { addNewUser } = require('./controller/addNewUser');
-
+const express = require("express");
+const { addNewUser } = require("./controller/addNewUser");
+const { loginUser } = require("./controller/loginUser");
 
 const userRouter = express.Router();
 
+userRouter.post("/add-user", addNewUser);
+userRouter.post("/login", loginUser);
 
-userRouter.post("/add-user", addNewUser)
-
-
-
-module.exports = {userRouter};
+module.exports = { userRouter };
