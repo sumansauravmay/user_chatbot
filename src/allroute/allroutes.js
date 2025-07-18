71bc0726ab5333ff02bdb204030a6dd83a41a983
Jyoti Ranjan <107979908+JyotiranjanGhibila@users.noreply.github.com>
2025-07-18 08:21:42 +0530

    modified registration api

--- src/allroute/allroutes.js
+++ src/allroute/allroutes.js
@@ -2,7 +2,7 @@
 
 
 const express = require('express');
-const { userRouter } = require('./users/user.route');
+const { userRouter } = require('../module/users/user.route');
 const allRoutes = express.Router();
 
 allRoutes.use('/auth', userRouter);
